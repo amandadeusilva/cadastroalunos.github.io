499a8b9ca45596bda30226ae013f17ecaf825e0f
Amanda <107001164+amandadeusilva@users.noreply.github.com>
2023-05-26 18:48:00 -0300

    commit inicial

---
+++ src/app/alunos/aluno.model.ts
@@ -0,0 +1,5 @@
+export class AlunoModel{
+    nome: string ='';
+    idade: number | undefined;
+    nota: number | undefined;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from  '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { AlunosService} from './alunos.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClientModule,AlunosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
